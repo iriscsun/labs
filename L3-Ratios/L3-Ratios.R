4b5da3a98cdb0f9f380f0901d43d4e6f761f7749
Iris Sun <iriscsun@gmail.com>
2017-01-25 10:19:39 -0800

    tried to do lab

--- L3-Ratios/L3-Ratios.R
+++ L3-Ratios/L3-Ratios.R
@@ -1,7 +1,9 @@
 #Welcome to Lab 3, please answer the following questions
 
 #Install and require package MASS
-
+install.packages("MASS")
+require(MASS)
+require(dplyr)
 
 #Using the built in dataset anorexia, aggregate the data to look at:
 #       1. the number of patients who received a treatment (choose one) who responded positively to treatment
@@ -10,7 +12,22 @@
 #       4. the number of patients who were in the control group who did not respond or responded negatively to treatment
 
 #Such that you end up with a 2 by 2 table
+View(anorexia)
 
+calculations <- mutate(anorexia, change = anorexia$Postwt - anorexia$Prewt)
+
+positive.change <- filter(calculations, change > 0, Treat == "CBT")
+negative.change <- filter(calculations, change <= 0, Treat == "CBT")
+
+control.positive <- filter(positive.change, Treat == "Cont")
+control.negative <- filter(negative.change, Treat == "Cont")
+  
+CBT <- c(nrow(positive.change), nrow(control.positive))
+Control <- c(nrow(negative.change), nrow(control.negative))
+
+table <- data.frame(CBT, Control)
+rownames(table)[1] <- "Positive Change"
+rownames(table)[2] <- "Negative Change"
 
 #What kind of analysis appropriate to perform on a study like this?
 
